d427abddf080d0240efcf73c40b5f2cf4a26c8e1
h-koichiro <h.koichiro0206@gmail.com>
2023-11-10 21:36:41 +0900

    型定義終了

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,10 @@
-type problem = {
+export type problem = {
 	text:string,
 	testcase:Testcase,
 	code:string,
 }
 
-type Testcase = {
+export type Testcase = {
     input: string[],
 	output: string[],
-}
-
-const getProblem = (id: Number): problem => {
-	return(
-        
-    );
 }
