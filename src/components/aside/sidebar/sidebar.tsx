4ae4a553dfbe6efb39ad3bdbecb507b959a87b89
h-koichiro <h.koichiro0206@gmail.com>
2023-11-10 15:48:29 +0900

    「propsでデータを渡すようにして、mapで表示させるようにできますか？」への対応

--- src/components/aside/sidebar/sidebar.tsx
+++ src/components/aside/sidebar/sidebar.tsx
@@ -1,34 +1,17 @@
 import SidebarStyle from './sidebar.module.css';
 import Image from 'next/image'
-import CodeImage from '/public/imags/terminal-w.svg';
-import SchoolImag from '/public/imags/school-w.svg';
-import SettingImage from '/public/imags/settings-w.svg';
-import AccountImage from '/public/imags/account-w.svg';
-import ProblemImage from '/public/imags/view_list-w.svg';
 
-export const Sidebar = () => {
+export const Sidebar = (props: { sidebar: any[]; }) => {
     return (
         <div className={SidebarStyle.sidebar}>
-            <a href="#">
-                <Image className={SidebarStyle.icon} src={CodeImage} alt="code"></Image>
-                <h3 className={SidebarStyle.h3}>コード</h3>
-            </a>
-            <a href="#" className={SidebarStyle.active}>
-                <Image className={SidebarStyle.icon} src={SchoolImag} alt="school"></Image>
-                <h3 className={SidebarStyle.h3}>コース</h3>
-            </a>
-            <a href="#">
-                <Image className={SidebarStyle.icon} src={SettingImage} alt="setting"></Image>
-                <h3 className={SidebarStyle.h3}>設定</h3>
-            </a>
-            <a href="#">
-                <Image className={SidebarStyle.icon} src={AccountImage} alt="account"></Image>
-                <h3 className={SidebarStyle.h3}>アカウント</h3>
-            </a>
-            <a href="#">
-                <Image className={SidebarStyle.icon} src={ProblemImage} alt="problem"></Image>
-                <h3 className={SidebarStyle.h3}>問題一覧</h3>
-            </a>
+            {props.sidebar.map((item) => {
+                return (
+                    <a href="#" className={item.active}>
+                        <Image className={SidebarStyle.icon} src={item.path} alt={item.h3}></Image>
+                        <h3 className={SidebarStyle.h3}>{item.h3}</h3>
+                    </a>
+                );
+        })}
         </div>
     );
   }
