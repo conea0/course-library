20eaf47a2b8fe9ecaa9ddd5d6a2e287745028afa
h-koichiro <h.koichiro0206@gmail.com>
2023-11-10 16:42:41 +0900

    「問題文とテストケースの型定義をする」これ、何やってんのかまじでわからんです

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+type problem = {
+	text:string,
+	testcase:Testcase,
+	code:string,
+}
+
+type Testcase = {
+    input: string[],
+	output: string[],
+}
+
+const getProblem = (id: Number): problem => {
+	return(
+        
+    );
+}
