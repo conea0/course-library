dec6f08ca0460857264d45e6efec690d43ceabcc
h-koichiro <h.koichiro0206@gmail.com>
2023-11-12 17:04:00 +0900

    getProblemの引数をidだけではなく、パート、ユニット、問題番号にした

--- src/feature/problem/getProblem.ts
+++ src/feature/problem/getProblem.ts
@@ -7,14 +7,18 @@ const example: problem = load(
   readFileSync(resolve("src/feature/problem/problem.yaml"), "utf8")
 ) as problem;
 const data = [
-  {
-    statement: example.statement,
-    testcase: example.testcase,
-    code: example.code,
-  },
+  [
+    [
+      {
+        statement: example.statement,
+        testcase: example.testcase,
+        code: example.code,
+      },
+    ],
+  ],
 ];
-export const getProblem = (id: number): problem => {
+export const getProblem = (part: number, unit: number, id: number): problem => {
     return(
-        data[id]
+        data[part][unit][id]
     );
 }
