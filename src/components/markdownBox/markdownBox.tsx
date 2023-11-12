ab18128bb00f5cff71f596b39511cde5f5b14895
h-koichiro <h.koichiro0206@gmail.com>
2023-11-12 17:18:22 +0900

    part>unit>問題番号に対応

--- src/components/markdownBox/markdownBox.tsx
+++ src/components/markdownBox/markdownBox.tsx
@@ -4,7 +4,7 @@ import { getProblem } from '@/feature/problem/getProblem';
 import 'github-markdown-css/github-markdown-light.css';
 
 export const ProblemCard = () => {
-  const markdown = getProblem(0).statement;
+  const markdown = getProblem(0,0,0).statement;
   return (
     <article className={markdownBoxStyle.cardbox}>
       <ReactMarkdown className="markdown-body">
