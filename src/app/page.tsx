93f81f43fec809c32a11608df52d6a447d893314
0827MK <0327.kotokoto@gmail.com>
2023-11-14 15:14:33 +0900

    コンフリクトの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import "./globals.css"; // アプリケーションのスタイルをインポート
-import { LoginForm } from '@/components/Login/LoginForm';
-import StudentListPage from "@/app/components/StudentForm/StudentListPage";
+import { LoginForm } from "@/components/Login/LoginForm";
+import StudentListPage from "./components/StudentForm/StudentListPage";
 
 export default function Home() {
   return (
