69de98e60647d087e5f6e6279528fee9f6950ed7
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-12-18 17:46:21 +0900

    不要なコメントを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { ProblemBox } from '@/components/problemBox/problemBox';
-import './globals.css'; // アプリケーションのスタイルをインポート
-import { LoginForm } from '@/components/Login/LoginForm';
+import { ProblemBox } from "@/components/problemBox/problemBox";
+import "./globals.css"; // アプリケーションのスタイルをインポート
 
 export default function Home() {
   return (
     <div>
-      {/* <LoginForm /> */}
       <ProblemBox part={0} unit={0} id={0} />
-
     </div>
   );
 }
