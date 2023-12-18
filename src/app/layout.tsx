9132a9ff24137fdaad58bee784fb5a7f7914b061
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-12-18 17:46:09 +0900

    Reactをimport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,26 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import {Aside} from '@/components/aside/aside';
-const inter = Inter({ subsets: ['latin'] })
+import { Aside } from "@/components/aside/aside";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import React from "react";
+import "./globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-  title: 'CONEA',
-  description: 'Course - Library',
-}
+  title: "CONEA",
+  description: "Course - Library",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
       <body className={inter.className}>
         <Aside />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
       </body>
     </html>
-  )
+  );
 }
