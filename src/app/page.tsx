fdf61d9a4b57c268b2d6378d4036faf8223ce69b
harashina <h.koichiro0206@gmail.com>
2023-12-21 20:36:06 +0900

    teisei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/Login/LoginForm";
 import "./globals.css"; // アプリケーションのスタイルをインポート
 export default function Home() {
-  return <Pyscript />;
+  return <LoginForm />;
 }
